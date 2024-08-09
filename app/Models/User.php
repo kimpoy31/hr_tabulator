5aaa2284040e28a1300a6818dc19cf328fb836c6
Kim Harold <pinanonangkim000@gmail.com>
2024-08-09 10:13:28 -0700

    Fresh layout_2

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'role',
+        'username',
         'password',
     ];
 
@@ -29,7 +29,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -40,7 +39,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
