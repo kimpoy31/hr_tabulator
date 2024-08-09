38a9b740f6e58e365bfd1950eb2fb02f2aa9622e
Kim Harold <pinanonangkim000@gmail.com>
2024-08-09 13:07:47 -0700

    Layout create activity modal

--- resources/js/Modals/NewActivityModal.tsx
+++ resources/js/Modals/NewActivityModal.tsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NewActivityModal = () => {
+    const [activity, setActivity] = useState<string>('')
+    const [description, setDescription] = useState<string>('')
+
     const openModal = () => {
         const modal = document.getElementById('my_modal_3');
         if (modal) {
@@ -8,19 +11,53 @@ const NewActivityModal = () => {
         }
     };
 
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+    }
+
   return (
     <>
         {/* You can open the modal using document.getElementById('ID').showModal() method */}
         <button className="btn btn-primary btn-sm" onClick={()=>openModal()}>New Activity</button>
         
         <dialog id="my_modal_3" className="modal">
-            <div className="modal-box">
+            <div className="modal-box max-w-sm">
                 <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+                    {/* if there is a button in form, it will close the modal */}
+                    <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
                 </form>
-                <h3 className="font-bold text-lg">Hello!</h3>
-                <p className="py-4">Press ESC key or click on ✕ button to close</p>
+                <h3 className="font-bold text-lg">New Activity</h3>
+
+
+                <form className="card-body p-0" onSubmit={(e) => onSubmit(e)}>
+                    {/* <h2 className="card-title"></h2> */}
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text">Activity name</span>
+                        </div>
+                        <input 
+                            type="text" 
+                            value={activity}
+                            onChange={(e) => setActivity(e.target.value)}
+                            className="input input-bordered w-full" 
+                        />
+                    </label>
+                    <label className="form-control w-full">
+                        <div className="label">
+                            <span className="label-text">Description</span>
+                        </div>
+                        <textarea 
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)}
+                            className="textarea textarea-bordered w-full" 
+                        />
+                    </label>
+
+                    <div className="card-actions justify-end mt-2">
+                        <button className="btn btn-primary w-full">Create</button>
+                    </div>
+                </form>
+
             </div>
         </dialog>
     </>
