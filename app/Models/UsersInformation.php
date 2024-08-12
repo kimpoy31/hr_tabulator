879f44f0942981d33dd6a0253297da0d053de927
Kim Harold <pinanonangkim000@gmail.com>
2024-08-12 11:16:52 -0700

    Refactor UsersInformation Model

--- app/Models/UsersInformation.php
+++ app/Models/UsersInformation.php
@@ -7,5 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class UsersInformation extends Model
 {
+    protected $fillable = [
+        'user_id',
+        'activity_id',
+        'fullname',
+        'role',
+        'status',
+    ];
+
     use HasFactory;
 }
--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -2,6 +2,15 @@ export interface User {
     id: number;
     role: string;
     username: string;
+    userInformation:UserInformation;
+}
+
+export interface UserInformation {
+    user_id: number;
+    activity_id: number;
+    fullname: string;
+    role: string;
+    status: 'active'|'inactive';
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
