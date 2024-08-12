4d7c99e98dc6b68d2ae7e8fcf187fb35c0f7c840
Kim Harold <pinanonangkim000@gmail.com>
2024-08-12 16:54:29 -0700

    create criteria model and migration file

---
+++ database/migrations/2024_08_12_234630_create_criterias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('criterias', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('activity_id');
+            $table->string('criteria');
+            $table->integer('percentage');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('criterias');
+    }
+};
