73b13caf37cbdc5225a1084a4343f0a00033ef08
Kim Harold <pinanonangkim000@gmail.com>
2024-08-15 10:18:07 -0700

    Add scores on contesant creation

--- app/Http/Controllers/ContestantController.php
+++ app/Http/Controllers/ContestantController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Contestant;
+use App\Models\Criteria;
+use App\Models\Score;
+use App\Models\UsersInformation;
 
 class ContestantController
 {
@@ -14,6 +17,38 @@ class ContestantController
         ]);
 
         $createdContestant = Contestant::create($validatedData);
+
+        if($createdContestant){
+
+            $judges = UsersInformation::where('activity_id', $createdContestant->activity_id)
+                    ->where('status','active')
+                    ->get();
+
+            $contestants = Contestant::where('activity_id', $createdContestant->activity_id)
+                    ->where('status', 'active')
+                    ->get();
+
+            $criterias = Criteria::where('activity_id', $createdContestant->activity_id)
+                    ->where('status', 'active')
+                    ->get();
+
+            if($judges->count() > 0 && $contestants->count() > 0 && $criterias->count() > 0 ){
+                foreach($judges as $judge){
+                    foreach($criterias as $criteria){
+                        Score::create(
+                            [
+                                'activity_id' => $createdContestant->activity_id,
+                                'judge_id' => $judge->user_id,
+                                'criteria_id' => $criteria->id,
+                                'contestant_id' => $createdContestant->id,
+                                'score' => 0,
+                            ]
+                        );
+                    }
+                }
+            }
+
+        }
 
         return response()->json(['contestant' => $createdContestant], 201);
     }
