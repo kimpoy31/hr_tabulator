ecb56c3b82678196d55d842d6ea27623bf96319f
Kim Harold <pinanonangkim000@gmail.com>
2024-08-30 08:56:27 -0700

    Refactor DefaultLayout design

--- resources/js/Components/Navbar.tsx
+++ resources/js/Components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div 
-        className="navbar bg-base-100 shadow-lg px-32 md:px-16 flex justify-between">
+        className="navbar bg-base-100 shadow-lg lg:px-32 px-4 md:px-16 flex justify-between">
         <div className='uppercase text-2xl font-bold'>HR Tabulator</div>
         {props.auth.user &&
             <Link href={route('logout')} className='btn lg:btn-sm btn-xs btn-outline btn-error'><MdLogout size={18}/> Logout</Link>
--- resources/js/Layouts/DefaultLayout.tsx
+++ resources/js/Layouts/DefaultLayout.tsx
@@ -8,12 +8,26 @@ interface DefaultLayoutProps extends PropsWithChildren<{}> {
 const DefaultLayout = ({ children, className }: DefaultLayoutProps) => {
   return (
     <div>
-        <header>
+        <header className='absolute w-full'>
           <Navbar />
         </header>
-        <main className={className}>
-            { children }
+        <main 
+          className={`${className} h-screen pt-28 `}
+          style={{
+            backgroundImage: 'url("/images/pcsaBG.png")',
+            backgroundSize: 'cover', // 'cover' or 'contain', depending on your needs
+            backgroundPosition: 'center', // adjusts the image position
+            backgroundRepeat: 'no-repeat', // prevents the image from repeating
+          }}
+        >
+          { children }
         </main>
+        <footer className="bg-base-200 w-full absolute bottom-0 lg:px-32 px-4 py-10 border-t-8">
+          <div className="flex gap-4">
+            <img src="/images/cscLogo.png" alt="cscLogo" className='lg:max-h-20 max-h-12' />
+            <img src="/images/themeLogo.png" alt="cscLogo" className='lg:max-h-20 max-h-12' />
+          </div>
+        </footer>
     </div>
   )
 }
